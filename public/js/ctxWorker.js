6a81839c1e10e2c7c5f6704cbb930f64eb28be99
Anwesh17 <anwesh.mishra@bankbazaar.com>
2017-02-06 18:50:54 +0530

    Extracting pixels from image

---
+++ public/js/ctxWorker.js
@@ -0,0 +1,5 @@
+self.onmessage = function(msg){
+    console.log(msg.data)
+    console.log(msg.data.socket)
+    console.log(msg.data.ctx)
+}
---
+++ public/js/index.js
@@ -0,0 +1,73 @@
+var canvas = document.getElementById('canvas-container')
+var ctx = canvas.getContext('2d')
+var time = 0
+var video = document.createElement('video')
+var socket = io.connect("http://localhost:8000/imageCarrier")
+var pixelHandler = {
+    isRunning:false,
+    handlePixel:function(){
+      new Promise((resolve,reject)=>{
+          var imageData = ctx.getImageData(0,0,canvas.width,canvas.height)
+          var data = imageData.data
+          var w = canvas.width
+          var h = canvas.height
+          console.log(h)
+          var pixels = []
+          var n = 0;
+          var pixel = new Array()
+          for(var i=0;i<data.length;i+=4) {
+              if(n%w == 0) {
+                pixels.push(pixel)
+                pixel = new Array()
+              }
+              else {
+                  pixel.push([data[2],data[1],data[0]])
+              }
+              n++;
+          }
+          resolve(pixels)
+      }).then((pixels)=>{socket.emit('draw',pixels)})
+    }
+}
+var ctxWorker = new Worker('js/ctxWorker.js')
+socket.on('ack',(data)=>{
+    console.log("acknowledged")
+})
+video.autoplay = true
+window.onresize = () =>{
+    setDimensions()
+}
+var setDimensions = () => {
+    canvas.width = window.innerWidth/4
+    canvas.height = window.innerWidth/4
+    video.width = canvas.width
+    video.height = canvas.height
+}
+setDimensions()
+navigator.getUserMedia = navigator.getUserMedia||navigator.webkitGetUserMedia||navigator.mozGetUserMedia
+var successCallback = (stream) => {
+    video.src = window.URL.createObjectURL(stream)
+}
+var failureCallback = (err) => {
+    console.log(err)
+}
+
+navigator.getUserMedia({audio:false,video:true},successCallback,failureCallback)
+function clone(objectToBeCloned) {
+    if(!(objectToBeCloned instanceof Object)) {
+        return objectToBeCloned
+    }
+    var Constructor = objectToBeCloned.constructor
+    var objectToClone = new Constructor()
+    for(var prop in objectToBeCloned) {
+        objectToClone[prop] = clone(objectToCloned[prop])
+    }
+    console.log(objectToClone)
+    return objectToClone
+}
+setInterval(()=>{
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    ctx.drawImage(video,0,0,canvas.width,canvas.height)
+    pixelHandler.handlePixel()
+    time++
+},1)
